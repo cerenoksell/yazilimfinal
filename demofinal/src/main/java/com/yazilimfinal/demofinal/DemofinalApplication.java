7ca86d36f601db3303113064bd48b05c6d2acb13
bprlab <bprlab@10.200.144.37>
2019-06-18 16:07:20 +0300

    y

---
+++ demofinal/src/main/java/com/yazilimfinal/demofinal/DemofinalApplication.java
@@ -0,0 +1,13 @@
+package com.yazilimfinal.demofinal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemofinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemofinalApplication.class, args);
+	}
+
+}
---
+++ demofinal/src/test/java/com/yazilimfinal/demofinal/DemofinalApplicationTests.java
@@ -0,0 +1,16 @@
+package com.yazilimfinal.demofinal;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DemofinalApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
